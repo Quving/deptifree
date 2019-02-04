6288de7ad0e2ae4d9b1779bf51801c4992778ce3
Vinh Ngu <vinh-ngu@hotmail.com>
2019-02-04 19:07:39 +0100

    Server static files in production.

--- deptifree/settings.py
+++ deptifree/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '$g_k-yc##-^ok#xcqjk_tf70bm$rl&v-p!)js-rb_(#olmftdq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 # Application definition
 
@@ -136,6 +136,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
 
 # Configure Email-Backend
