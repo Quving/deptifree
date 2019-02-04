51f564985065efd065e72a1246174b63c1081188
Vinh Ngu <vinh-ngu@hotmail.com>
2019-02-04 17:37:12 +0100

    Fix db problems.

--- dept/models.py
+++ dept/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 
 
 class Dept(models.Model):
-    owner = models.ForeignKey(User, related_name='depts', on_delete=models.CASCADE, default=0)
+    owner = models.ForeignKey(User, related_name='depts', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=100,
                             blank=True,
