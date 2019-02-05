e3dfa1e7af3877a2348283ead9cfc7d6e83be299
Vinh Ngu <vinh-ngu@hotmail.com>
2019-02-05 19:31:39 +0100

    Add 'contactlists' attribute to user-model.

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from contactlist.models import ContactList
 from dept.models import Dept
 from .models import ApplicationUser
 
@@ -7,6 +8,8 @@ from .models import ApplicationUser
 class ApplicationUserSerializer(serializers.ModelSerializer):
     depts = serializers.PrimaryKeyRelatedField(many=True, queryset=Dept.objects.all())
 
+    contactlists = serializers.PrimaryKeyRelatedField(many=True, queryset=ContactList.objects.all())
+
     class Meta:
         model = ApplicationUser
-        fields = ('id', 'email', 'username', 'name', 'depts')
+        fields = ('id', 'email', 'username', 'name', 'depts', 'contactlists')
